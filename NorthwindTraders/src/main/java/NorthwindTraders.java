c77b4866e9d202686f41a81b90abaf71495e0483
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-06-10 09:13:41 -0400

    completed extended version

--- NorthwindTraders/src/main/java/NorthwindTraders.java
+++ NorthwindTraders/src/main/java/NorthwindTraders.java
@@ -9,13 +9,20 @@ public class NorthwindTraders {
                 "password");
 
         Statement statement = connection.createStatement();
-        String query = "SELECT ProductName FROM products";
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
 
         ResultSet results = statement.executeQuery(query);
 
         while (results.next()) {
             String productName = results.getString("ProductName");
+            int productId = results.getInt("ProductID");
+            double unitPrice = results.getDouble("UnitPrice");
+            int unitsInStock = results.getInt("UnitsInStock");
             System.out.println(productName);
+            System.out.println("Product ID: " + productId);
+            System.out.println("Unit Price: $" + unitPrice);
+            System.out.println("Units in Stock: " + unitsInStock);
+            System.out.println("----------------");
         }
         connection.close();
 
