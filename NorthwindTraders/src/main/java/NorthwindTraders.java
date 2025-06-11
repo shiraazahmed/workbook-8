9045f847ba12fb9bb3877fb5133fb1628b5f7c29
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-06-11 13:56:50 -0400

    completed

--- NorthwindTraders/src/main/java/NorthwindTraders.java
+++ NorthwindTraders/src/main/java/NorthwindTraders.java
@@ -1,31 +1,85 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class NorthwindTraders {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =  DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/Northwind",
-                "userName",
-                "password");
+        Scanner scanner = new Scanner(System.in);
+        Connection connection = null;
 
-        Statement statement = connection.createStatement();
-        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Northwind",
+                    "root",
+                    "yearup");
 
-        ResultSet results = statement.executeQuery(query);
+            while (true) {
+                System.out.println("Choose an option:");
+                System.out.println("1: Display all products");
+                System.out.println("2: Display all customers");
+                System.out.println("3: Display all categories");
+                System.out.println("0: Exit");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-        while (results.next()) {
-            String productName = results.getString("ProductName");
-            int productId = results.getInt("ProductID");
-            double unitPrice = results.getDouble("UnitPrice");
-            int unitsInStock = results.getInt("UnitsInStock");
-            System.out.println(productName);
-            System.out.println("Product ID: " + productId);
-            System.out.println("Unit Price: $" + unitPrice);
-            System.out.println("Units in Stock: " + unitsInStock);
-            System.out.println("----------------");
+                if (choice == 1) {
+                    String query1 = "SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products";
+                    Statement statement = connection.createStatement();
+                    ResultSet results = statement.executeQuery(query1);
+                    while (results.next()) {
+                        System.out.println("ProductId: " + results.getInt("ProductId"));
+                        System.out.println("ProductName: " + results.getString("ProductName"));
+                        System.out.println("UnitPrice: $" + results.getDouble("UnitPrice"));
+                        System.out.println("UnitsInStock: " + results.getInt("UnitsInStock"));
+                        System.out.println("----------------");
+                    }
+                } else if (choice == 2) {
+                    String query2 = "SELECT ContactName, CompanyName, Phone, Region, City FROM Customers ORDER BY Country";
+                    try (Statement statement = connection.createStatement();
+                         ResultSet results = statement.executeQuery(query2)) {
+                        while (results.next()) {
+                            System.out.println("Contact: " + results.getString("ContactName"));
+                            System.out.println("Company: " + results.getString("CompanyName"));
+                            System.out.println("Number: " + results.getString("Phone"));
+                            System.out.println("Region: " + results.getString("Region"));
+                            System.out.println("City: " + results.getString("City"));
+                            System.out.println("----------------");
+                        }
+                    }
+                } else if (choice == 3) {
+                    String query3 = "SELECT * FROM Categories ORDER BY CategoryId";
+                    try (Statement statement = connection.createStatement();
+                         ResultSet results = statement.executeQuery(query3)) {
+                        while (results.next()) {
+                            System.out.println("CategoryId: " + results.getInt("CategoryId"));
+                            System.out.println("CategoryName: " + results.getString("CategoryName"));
+                            System.out.println("----------------");
+                        }
+                    }
+                    System.out.println("Type your CategoryId ");
+                    int categoryId = scanner.nextInt();
+                    scanner.nextLine();
+                } else if (choice == 0) {
+                    System.out.println("Exiting...");
+                    break;
+                } else {
+                    System.out.println("Invalid choice, please try again.");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error:");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error!");
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            scanner.close();
         }
-        connection.close();
-
-
     }
 }
+        
+
+
+
+
