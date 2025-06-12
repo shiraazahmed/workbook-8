9e07c827b4005227a346237389813b6f92873533
shiraazahmed <shiraaza08@gmail.com>
2025-06-12 10:05:44 -0400

    Add files via upload

--- Craig-Examples/untitled/src/main/java/Main.java
+++ Craig-Examples/untitled/src/main/java/Main.java
@@ -1,6 +1,10 @@
 package com.pluralsight;
 
-import java.sql.*;
+import com.pluralsight.data.FilmDAO;
+import com.pluralsight.model.Film;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,49 +13,47 @@ public class Main {
     public static void main(String[] args) {
         String username = args[0];
         String password = args[1];
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+        displayAllFilmsScreen(dataSource);
+        displayFilmSearchScreen(dataSource);
+
+    }
+
+    private static void displayAllFilmsScreen(BasicDataSource dataSource) {
+        FilmDAO filmDAO = new FilmDAO(dataSource);
+        List<Film> films = filmDAO.getAll();
+
+        //print header row
+        System.out.printf("%-4s %-40s %10s%n", "Id", "Title", "Release Year");
+        System.out.println("_________________________________________________________________________________");
+
+        for (Film film:films){
+            System.out.printf("%-4d %-40s %10d%n", film.getFilmId(), film.getTitle(), film.getReleaseYear());
+        }
+    }
+
+    private static void displayFilmSearchScreen(BasicDataSource dataSource) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Search for films that start with: ");
         String searchTerm = scanner.nextLine() + "%";
 
-        String sql = """
-                 select * 
-                 from film
-                 where title like ?;
-                """;
+        FilmDAO filmDAO = new FilmDAO(dataSource);
+        List<Film> films = filmDAO.search(searchTerm);
 
-        try (
-                //create a connection
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", username, password);
-                //create a SQL statement
-                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        //print header row
+        System.out.printf("%-4s %-40s %10s%n", "Id", "Title", "Release Year");
+        System.out.println("_________________________________________________________________________________");
 
-        ) {
-
-            //execute the statement/query
-            preparedStatement.setString(1, searchTerm);
-
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                //print header row
-                System.out.printf("%-4s %-40s %10s%n", "Id", "Title", "Release Year");
-                System.out.println("_________________________________________________________________________________");
-
-                //loop through the results and display them
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("film_id");
-                    String title = resultSet.getString("title");
-                    int releaseYear = resultSet.getInt("release_year");
-
-                    //print row
-                    System.out.printf("%-4d %-40s %10d%n", id, title, releaseYear);
-                }
-            }
-
-        } catch (SQLException e) {
-            //display user friendly error message
-            System.out.println("There was an error retrieving the films. Please try again or contact support.");
-            //display error message for the developer
-            e.printStackTrace();
+        for (Film film:films){
+            System.out.printf("%-4d %-40s %10d%n", film.getFilmId(), film.getTitle(), film.getReleaseYear());
         }
-
     }
+
+
 }
